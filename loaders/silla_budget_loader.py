9e28c259671206fb8369d8dfbdee33fc1ec3d199
Eduardo Sebastian <eduardo@civio.es>
2016-12-05 08:11:41 +0100

    Add programmes mapping for 2009 budget

--- loaders/silla_budget_loader.py
+++ loaders/silla_budget_loader.py
@@ -46,6 +46,119 @@ class SillaBudgetLoader(SimpleBudgetLoader):
             '4310': '4312',     # Mercados, abastos y lonjas
         }
 
+        # Programme codes have changed also in 2010. We are forced to amend budget data prior to 2010
+        # in order to mantain the code-programme mapping constant.
+        programme_mapping_2010 = {
+            # old programme: new programme
+            '1110': '9120',     # Órganos de gobierno
+            '1111': '9121',     # Grupos Políticos
+            '1112': '9120',     # Órganos de gobierno
+            '1113': '9122',     # Gabinete de comunicación
+            '1114': '9120',     # Órganos de gobierno
+            '1210': '9200',     # Servicios Generales
+            '1211': '9201',     # Secretaría
+            '1212': '9202',     # Oficinas Municipales
+            '1213': '9203',     # Edificios Municipales
+            '1214': '9204',     # Departamento informática
+            '1215': '9203',     # Edificios Municipales
+            '1216': '9206',     # Bienes Municipales
+            '1217': '9207',     # Juzgado
+            '1218': '9292',     # Taller
+            '1219': '9209',     # Departamento de Personal
+            '1230': '9205',     # Asesoría y defensa letrada
+            '1240': '9120',     # Órganos de gobierno
+            '2220': '1320',     # Seguridad y orden público
+            '2221': '1330',     # Control del tráfico
+            '2222': '1331',     # Recogida de vehículos
+            '3110': '2200',     # Otras prestaciones económicas a favor de empleados
+            '3120': '2210',     # Personal activo
+            '3130': '2312',     # Asistencia domiciliaria
+            '3131': '2311',     # Acción Social
+            '3132': '2310',     # Asistencia Social
+            '3133': '2315',     # Departamento de la mujer
+            '3135': '3379',     # Centro Tercera Edad
+            '3136': '3379',     # Centro Tercera Edad
+            '3139': '2316',     # Centro de día para menores
+            '3210': '2318',     # Educador de Calle
+            '3220': '2410',     # Escuelas Taller
+            '3222': '2411',     # Promoción del empleo
+            '3223': '4300',     # Promoción Económica
+            '3230': '2314',     # Entidades no lucrativas
+            '3224': '2412',     # Bolsa de empleo
+            '3233': '2313',     # Prevención drogodependencias
+            '4110': '3111',     # Salud Pública
+            '4120': '3121',     # Servicio Médico
+            '4131': '3111',     # Salud Pública
+            '4132': '3112',     # Desinfeccion y desratización
+            '4134': '3113',     # Ciudades sanas
+            '4220': '3231',     # Enseñanza Primaria
+            '4221': '3200',     # Ponencia de educación
+            '4223': '3261',     # Escuela Permanente de Adultos
+            '4226': '3265',     # OOAA Conservatorio Músic
+            '4230': '3264',     # Gabinete psicopedagógico
+            '4260': '2317',     # Comedor social
+            '4310': '9203',     # Edificios Municipales
+            '4312': '1521',     # Agencia de la vivienda
+            '4314': '1522',     # Plan especial rehabilitación
+            '4320': '1510',     # Urbanismo
+            '4321': '1510',     # Urbanismo
+            '4322': '1510',     # Urbanismo
+            '4323': '1532',     # Vías públicas
+            '4340': '1650',     # Alumbrado Público
+            '4351': '1710',     # Parques y Jardines
+            '4352': '1710',     # Parques y Jardines
+            '4410': '1610',     # Servicio de Agua Potable
+            '4411': '1600',     # Alcantarillado y tratamiento de aguas
+            '4413': '1612',     # Fuente del Manano
+            '4420': '1630',     # Limpieza viaria
+            '4422': '1621',     # Recogida de residuos
+            '4424': '1621',     # Recogida de residuos
+            '4430': '1640',     # Cementerio Municipal
+            '4440': '1532',     # Vías públicas
+            '4441': '1531',     # Obras
+            '4442': '1531',     # Obras
+            '4460': '1700',     # Medio Ambiente
+            '4461': '1700',     # Medio Ambiente
+            '4470': '4930',     # O.M.I.C.
+            '4480': '4312',     # Mercados, abastos y lonjas
+            '4510': '3320',     # Bibliotecas y Archivos
+            '4511': '3343',     # Campaña animación lectura
+            '4512': '3344',     # Proyectos culturales Europeos
+            '4513': '3265',     # OOAA Conservatorio Música
+            '4514': '3330',     # Escuela de Teatro
+            '4515': '3300',     # Animador Cultural
+            '4516': '3340',     # Ponencia de Cultura
+            '4517': '3341',     # Casa de la Cultura
+            '4518': '3370',     # Ponencia de Juventud
+            '4519': '3347',     # Promoción de uso Valenciano
+            '4520': '3422',     # Piscinas municipales
+            '4521': '3421',     # Polideportivo Municipal
+            '4522': '3410',     # Ponencia de Deportes
+            '4531': '3360',     # Arqueología y protección del patrimonio histórico-artístico
+            '4540': '3380',     # Fiestas Populares
+            '4610': '3345',     # Bandas de Música
+            '4631': '4910',     # Medios de comunicación
+            '4640': '3381',     # Junta Local Fallera
+            '4641': '3382',     # Comisiones Falleras
+            '4650': '2314',     # Entidades no lucrativas
+            '4651': '3370',     # Ponencia de Juventud
+            '4653': '2314',     # Entidades no lucrativas
+            '5110': '1532',     # Vías públicas
+            '5111': '1532',     # Vías públicas
+            '5112': '1532',     # Vías públicas
+            '5310': '4120',     # Plan Ecológico ganadería y agricultura
+            '6110': '9310',     # Área Económica
+            '6113': '9320',     # Recaudación Tributos
+            '6220': '4311',     # Feria San Sebastián
+            '6320': '9240',     # Participación ciudadana
+            '7110': '4100',     # Consejo Local Agrario
+            '7111': '4100',     # Consejo Local Agrario
+            '9112': '9420',     # Transferencias a Entidades Locales territoriales
+            '9114': '9430',     # Transferencias a otras Entidades Locales
+            '9116': '9430',     # Transferencias a otras Entidades Locales
+            '9210': '9291',     # Indemnizaciones
+        }
+
         # Some dirty lines in input data
         if line[0]=='':
             return None
@@ -58,9 +171,11 @@ class SillaBudgetLoader(SimpleBudgetLoader):
             # We got 3- or 5- digit economic codes as input, so add a trailing zero
             ec_code = line[2].ljust(5, '0')
 
-            # For years before 2015 we check whether we need to amend the programme code
+            # For years before 2009 and 2015 we check whether we need to amend the programme code
             year = re.search('municipio/(\d+)/', filename).group(1)
-            if int(year) < 2015:
+            if int(year) < 2010:
+                fc_code = programme_mapping_2010.get(fc_code, fc_code)
+            elif int(year) < 2015:
                 fc_code = programme_mapping.get(fc_code, fc_code)
 
             return {
