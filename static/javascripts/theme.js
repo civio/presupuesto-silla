8bf0e29c6cd793edfe55f0951bad4097aa164c43
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2017-02-14 17:20:49 +0100

    civio/presupuesto-management#411

---
+++ static/javascripts/theme.js
@@ -0,0 +1,39 @@
+// Theme custom js methods
+$(document).ready(function(){
+
+  // Add treemap alert
+  var addChartsAlert = function() {
+    var str = {
+      chapter: {
+        'es': 'Ingresos por capítulo',
+        'ca': 'Ingresos por capítulo'
+      },
+      article: {
+        'es': 'Ingresos por artículo',
+        'ca': 'Ingresos por artículo'
+      },
+    };
+
+    $('.policies-chart #budget-summary').prepend('<div class="alert alert-incomes">'+str.chapter[ $('html').attr('lang') ]+'</div>');
+    $('.policies-chart #budget-summary').append('<div class="alert alert-incomes">'+str.article[ $('html').attr('lang') ]+'</div>');
+     
+  };
+
+  // show / hide treemap alert based on selected tab
+  var setupChartsAlert = function(state) {
+    if (state == 'income') {
+      $('.policies-chart #budget-summary .alert-incomes').show();
+    } else {
+      $('.policies-chart #budget-summary .alert-incomes').hide();
+    }
+  };
+
+  addChartsAlert();
+
+  setupChartsAlert($('section').data('tab'));
+
+  $(window).bind('hashchange', function(e) {
+    var state = $.deparam.fragment();
+    setupChartsAlert(state.view);
+  });
+});
